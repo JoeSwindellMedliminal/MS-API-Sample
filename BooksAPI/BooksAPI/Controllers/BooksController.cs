89eae3693ee8d3d4d29729202293a8658697fd9f
Joe Swindell <joes@medliminal.com>
2020-01-21 10:45:12 -0500

    Done and working

---
+++ BooksAPI/BooksAPI/Controllers/BooksController.cs
@@ -0,0 +1,123 @@
+﻿using BooksAPI.Data;
+using BooksAPI.DTOs;
+using BooksAPI.Models;
+using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace BooksAPI.Controllers
+{
+    [RoutePrefix("api/books")]
+    public class BooksController : ApiController
+    {
+
+        private BooksAPIContext db = new BooksAPIContext();
+
+        //Typed lambda expression for Select() method.
+        private static readonly Expression<Func<Book, BookDto>> AsBookDto =
+            x => new BookDto
+            {
+                Title = x.Title,
+                Author = x.Author.Name,
+                Genre = x.Genre
+            };
+
+        // GET: api/Books
+        // 	http://localhost/api/books
+        [Route("")]
+        public IQueryable<BookDto> GetBooks()
+        {
+            return db.Books.Include(b => b.Author).Select(AsBookDto);
+        }
+
+        // GET: api/Books/5
+        // http://localhost/api/books/5
+        [Route("{id:int}")]
+        [ResponseType(typeof(Book))]
+        public async Task<IHttpActionResult> GetBook(int id)
+        {
+            BookDto book = await db.Books.Include(b => b.Author)
+                .Where(b => b.BookId == id)
+                .Select(AsBookDto)
+                .FirstOrDefaultAsync();
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(book);
+        }
+
+        // GET: /api/books/{id}/details
+        // http://localhost/api/books/{id}/details
+        [Route("{id:int}/details")]
+        [ResponseType(typeof(BookDetailDto))]
+        public async Task<IHttpActionResult> GetBookDetails(int id)
+        {
+            var book = await (from b in db.Books.Include(b => b.Author)
+                              where b.BookId == id
+                              select new BookDetailDto
+                              {
+                                  Title = b.Title,
+                                  Genre = b.Genre,
+                                  PublishDate = b.PublishDate,
+                                  Price = b.Price,
+                                  Description = b.Description,
+                                  Author = b.Author.Name
+                              }).FirstOrDefaultAsync();
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return Ok(book);
+        }
+
+        // Get: api/books/fantasy
+        // http://localhost/api/books/fantasy
+        [Route("{genre}")]
+        public IQueryable<BookDto> GetBooksByGenre(string genre)
+        {
+            return db.Books.Include(b => b.Author)
+                .Where(b => b.Genre.Equals(genre, StringComparison.OrdinalIgnoreCase))
+                .Select(AsBookDto);
+        }
+
+        // Get: api/authors
+        // http://localhost/api/authors/{id}/books
+        [Route("~/api/authors/{authorId:int}/books")]
+        public IQueryable<BookDto> GetBooksByAuthor(int authorId)
+        {
+            return db.Books.Include(b => b.Author)
+                .Where(b => b.AuthorId == authorId)
+                .Select(AsBookDto);
+        }
+
+        // Get: api/books/date/yyyy-mm-dd -- Publication Date
+        // http://localhost/api/books/date/yyyy-mm-dd
+        // Get /api/books/date/yyyy/mm/dd -- notice slashes
+        // * indicates to routing engine to match the rest of the URI
+        // http://localhost/api/books/date/yyyy/mm/dd
+        [Route("date/{pubdate:datetime:regex(\\d{4}-\\d{2}-\\d{2})}")]
+        [Route("date/{*pubdate:datetime:regex(\\d{4}/\\d{2}/\\d{2})}")]
+        public IQueryable<BookDto> GetBooks(DateTime pubdate)
+        {
+            return db.Books.Include(b => b.Author)
+                .Where(b => DbFunctions.TruncateTime(b.PublishDate)
+                == DbFunctions.TruncateTime(pubdate))
+                .Select(AsBookDto);
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();          
+            base.Dispose(disposing);
+        }
+               
+    }
+}
